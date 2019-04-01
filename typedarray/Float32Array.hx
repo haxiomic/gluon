89600d90dca1759c4b64cf849cdbb9525389425c
George Corney <haxiomic@gmail.com>
2019-04-01 21:39:23 +0100

    Fixed typedarray bugs and used macro to create generic typedarray methods

--- typedarray/Float32Array.hx
+++ typedarray/Float32Array.hx
@@ -13,149 +13,25 @@ typedef Float32Array = js.lib.Float32Array;
 
 import typedarray.ArrayBufferView.ArrayBufferViewBase;
 import typedarray.ArrayBufferView.ArrayLike;
-import typedarray.ArrayBufferView.ArrayAccessImpl;
+import typedarray.ArrayBufferView.ArrayBufferViewImplIterator;
+
+private typedef ArrayType = Float;
 
 @:nullSafety
 @:forward
+#if !macro
+@:build(typedarray.ArrayBufferView.BuildArrayBufferView.build(ArrayType))
+#end
 abstract Float32Array(Float32ArrayImpl)
 	to Float32ArrayImpl from Float32ArrayImpl
  	to ArrayBufferView
-	to ArrayLike<Float>
+	to ArrayLike<ArrayType>
 {
-
-	static public inline var BYTES_PER_ELEMENT : Int = Float32ArrayImpl.BYTES_PER_ELEMENT;
-
-	/**
-		Call as either
-		- (length: Int)
-		- (copyFromArray: ArrayLike<Float>)
-		- (buffer: ArrayBuffer, byteOffset: Int, byteLength: Int)
-	**/
-	@:pure public inline function new(
-		?length: Int,
-
-		?copyFromArray: ArrayLike<Float>,
-
-		?buffer: ArrayBuffer,
-		?byteOffset: Int,
-		?byteLength: Int
-	) {
-		if (copyFromArray != null) {
-			this = cast Float32Array.from(copyFromArray);
-		} else {
-			this = new Float32ArrayImpl(
-				length,
-				buffer,
-				byteOffset,
-				byteLength
-			);
-		}
-	}
-
-	/*
-
-	# still todo
-	function copyWithin( target : Int, start : Int, ?end : Int ) : Float32Array;
-	function filter( callbackfn : Float -> Int -> Float32Array -> Dynamic, ?thisArg : Dynamic ) : Float32Array;
-	function find( predicate : Float -> Int -> Float32Array -> Bool, ?thisArg : Dynamic ) : Dynamic;
-	function findIndex( predicate : Float -> Int -> Float32Array -> Bool, ?thisArg : Dynamic ) : Int;
-	function lastIndexOf( searchElement : Float, ?fromIndex : Int ) : Int;
-	function map( callbackfn : Float -> Int -> Float32Array -> Float, ?thisArg : Dynamic ) : Float32Array;
-	@:overload( function( callbackfn : Float -> Float -> Int -> Float32Array -> Float ) : Int {} )
-	function reduce( callbackfn : Dynamic -> Float -> Int -> Float32Array -> Dynamic, initialValue : Dynamic ) : Dynamic;
-	@:overload( function( callbackfn : Float -> Float -> Int -> Float32Array -> Float ) : Int {} )
-	function reduceRight( callbackfn : Dynamic -> Float -> Int -> Float32Array -> Dynamic, initialValue : Dynamic ) : Dynamic;
-	function reverse() : Float32Array;
-	function slice( ?start : Int, ?end : Int ) : Float32Array;
-	function some( callbackfn : Float -> Int -> Float32Array -> Bool, ?thisArg : Dynamic ) : Bool;
-	function sort( ?compareFn : Float -> Float -> Int ) : Float32Array;
-
-	*/
-
-	public inline function set(copyFromArray: ArrayLike<Float>, ?offset: Int = 0): Void {
-		var i = 0;
-		for (v in copyFromArray) {
-			this.set(offset + i++, v);
-		}
-	}
-
-	public inline function join(?separator: String): String {
-		return [for (v in this) v].join(separator);
-	}
-
-	public inline function indexOf(searchElement: Float, ?fromIndex: Int = 0) : Int {
-		if (fromIndex < 0) fromIndex = this.length + fromIndex;
-		var result = -1;
-		for (i in fromIndex...this.length) {
-			if (this.get(i) == searchElement) {
-				result = i;
-				break;
-			}
-		}
-		return result;
-	}
-
-	public inline function subarray(start: Int, ?end: Int) {
-		if (end == null) end = this.length;
-		if (start < 0) start = this.length + start;
-		if (end < 0) end = this.length + end;
-		return new Float32Array(this.buffer, start * BYTES_PER_ELEMENT, (end - start) * BYTES_PER_ELEMENT);
-	}
-
-	public inline function fill(value: Float, ?start: Int = 0, ?end: Int) {
-		if (end == null) end = this.length;
-		if (start < 0) start = this.length + start;
-		if (end < 0) end = this.length + end;
-		for (i in 0...(end - start)) {
-			this.set(i + start, value);
-		}
-	}
-
-	public inline function forEach(callback: Float -> Int -> Float32Array -> Void): Void {
-		var i = 0;
-		for (v in this) {
-			callback(v, i++, this);
-		}
-	}
-
-	public inline function every(callback: Float -> Int -> Float32Array -> Bool): Bool {
-		var result: Bool = true;
-		var i = 0;
-		for (v in this) {
-			if (!callback(v, i++, this)) {
-				result = false;
-				break;
-			}
-		}
-		return result;
-	}
-
-	@:arrayAccess
-	@:pure inline function arrayGet(i: Int) {
-		return this.get(i);
-	}
-
-	@:arrayAccess
-	inline function arraySet(i: Int, v: Float) {
-		return this.set(i, v);
-	}
-
-	// @:pure static function of( items : haxe.extern.Rest<Array<Dynamic>> ) : Float32Array {}
-	@:pure static public inline function from(source: ArrayLike<Float>, ?mapFn: Float -> Int -> Float): Float32Array {
-		var view = new Float32Array(source.length);
-		var i = 0;
-		for (v in source) {
-			view[i] = mapFn != null ? mapFn(v, i) : v;
-			i++;
-		}
-		return view;
-	}
-
+	// typed filled by macro
 }
 
 @:noCompletion
 @:nullSafety
-@:allow(typedarray.Float32Array)
 class Float32ArrayImpl extends ArrayBufferViewBase {
 
 	static public inline var BYTES_PER_ELEMENT : Int = 4;
@@ -176,62 +52,45 @@ class Float32ArrayImpl extends ArrayBufferViewBase {
 		);
 	}
 
-	@:pure function get(i: Int) {
+	@:noCompletion
+	@:pure public inline function _get(i: Int): ArrayType {
 		#if debug
 		if (i < 0 || i >= this.length) {
 			throw haxe.io.Error.OutsideBounds;
 		}
 		#end
 
+		var p = i * BYTES_PER_ELEMENT + this.byteOffset;
+
 		#if cpp
-		return untyped __global__.__hxcpp_memory_get_float(this.nativeBytes, i * BYTES_PER_ELEMENT + this.byteOffset);
+		return untyped __global__.__hxcpp_memory_get_float(this.nativeBytes, p);
 		#else
-		return (this.buffer: haxe.io.Bytes).getFloat(i);
+		return (this.buffer: haxe.io.Bytes).getFloat(p);
 		#end
 	}
 
-	inline function set(i: Int, v: Float) {
+	@:noCompletion
+	public inline function _set(i: Int, v: ArrayType): ArrayType {
 		#if debug
 		if (i < 0 || i >= this.length) {
 			throw haxe.io.Error.OutsideBounds;
 		}
 		#end
 
+		var p = i * BYTES_PER_ELEMENT + this.byteOffset;
+
 		#if cpp
-		return untyped __global__.__hxcpp_memory_set_float(this.nativeBytes, i * BYTES_PER_ELEMENT + this.byteOffset, v);
+		untyped __global__.__hxcpp_memory_set_float(this.nativeBytes, p, v);
 		#else
-		return (this.buffer: haxe.io.Bytes).setFloat(i, v);
+		(this.buffer: haxe.io.Bytes).setFloat(p, v);
 		#end
+		return v;
 	}
 
-	@:pure public inline function iterator(): Iterator<Float> {
-		return new Float32ArrayIterator(this);
+	@:pure public inline function iterator() {
+		return new ArrayBufferViewImplIterator<ArrayType>(this);
 	}
 
-	function toString() {
-		return [for (i in 0...this.length) get(i)].join(',');
-	}
-
-}
-
-@:access(typedarray.Float32ArrayImpl)
-private class Float32ArrayIterator {
-
-	var a: Float32ArrayImpl;
-	var i: Int = 0;
-
-	public inline function new(array: Float32ArrayImpl) {
-		this.a = array;
-	}
-
-	public inline function hasNext(): Bool {
-		return i < (this.a.length);
-	}
-
-	public inline function next(): Float {
-		return a.get(i++);
-	}
-	
 }
 
 #end
--- typedarray/Uint8Array.hx
+++ typedarray/Uint8Array.hx
@@ -12,155 +12,30 @@ typedef Uint8Array = js.lib.Uint8Array;
 
 import typedarray.ArrayBufferView.ArrayBufferViewBase;
 import typedarray.ArrayBufferView.ArrayLike;
-import typedarray.ArrayBufferView.ArrayAccessImpl;
+import typedarray.ArrayBufferView.ArrayBufferViewImplIterator;
 
-#if !macro
-import cpp.UInt8;
+#if (!macro && cpp)
+private typedef UInt8 = cpp.UInt8;
 #else
-typedef UInt8 = Int;
+private typedef UInt8 = Int;
 #end
+
 
 @:nullSafety
 @:forward
+#if !macro
+@:build(typedarray.ArrayBufferView.BuildArrayBufferView.build(UInt8))
+#end
 abstract Uint8Array(Uint8ArrayImpl)
 	to Uint8ArrayImpl from Uint8ArrayImpl
  	to ArrayBufferView
 	to ArrayLike<UInt8>
 {
-
-	static public inline var BYTES_PER_ELEMENT : Int = Uint8ArrayImpl.BYTES_PER_ELEMENT;
-
-	/**
-		Call as either
-		- (length: Int)
-		- (copyFromArray: ArrayLike<UInt8>)
-		- (buffer: ArrayBuffer, byteOffset: Int, byteLength: Int)
-	**/
-	@:pure public inline function new(
-		?length: Int,
-
-		?copyFromArray: ArrayLike<UInt8>,
-
-		?buffer: ArrayBuffer,
-		?byteOffset: Int,
-		?byteLength: Int
-	) {
-		if (copyFromArray != null) {
-			this = cast Uint8Array.from(copyFromArray);
-		} else {
-			this = new Uint8ArrayImpl(
-				length,
-				buffer,
-				byteOffset,
-				byteLength
-			);
-		}
-	}
-
-	/*
-
-	# still todo
-	function copyWithin( target : Int, start : Int, ?end : Int ) : Uint8Array;
-	function filter( callbackfn : UInt8 -> Int -> Uint8Array -> Dynamic, ?thisArg : Dynamic ) : Uint8Array;
-	function find( predicate : UInt8 -> Int -> Uint8Array -> Bool, ?thisArg : Dynamic ) : Dynamic;
-	function findIndex( predicate : UInt8 -> Int -> Uint8Array -> Bool, ?thisArg : Dynamic ) : Int;
-	function lastIndexOf( searchElement : UInt8, ?fromIndex : Int ) : Int;
-	function map( callbackfn : UInt8 -> Int -> Uint8Array -> UInt8, ?thisArg : Dynamic ) : Uint8Array;
-	@:overload( function( callbackfn : UInt8 -> UInt8 -> Int -> Uint8Array -> UInt8 ) : Int {} )
-	function reduce( callbackfn : Dynamic -> UInt8 -> Int -> Uint8Array -> Dynamic, initialValue : Dynamic ) : Dynamic;
-	@:overload( function( callbackfn : UInt8 -> UInt8 -> Int -> Uint8Array -> UInt8 ) : Int {} )
-	function reduceRight( callbackfn : Dynamic -> UInt8 -> Int -> Uint8Array -> Dynamic, initialValue : Dynamic ) : Dynamic;
-	function reverse() : Uint8Array;
-	function slice( ?start : Int, ?end : Int ) : Uint8Array;
-	function some( callbackfn : UInt8 -> Int -> Uint8Array -> Bool, ?thisArg : Dynamic ) : Bool;
-	function sort( ?compareFn : UInt8 -> UInt8 -> Int ) : Uint8Array;
-
-	*/
-
-	public inline function set(copyFromArray: ArrayLike<UInt8>, ?offset: Int = 0): Void {
-		var i = 0;
-		for (v in copyFromArray) {
-			this.set(offset + i++, v);
-		}
-	}
-
-	public inline function join(?separator: String): String {
-		return [for (v in this) v].join(separator);
-	}
-
-	public inline function indexOf(searchElement: UInt8, ?fromIndex: Int = 0) : Int {
-		if (fromIndex < 0) fromIndex = this.length + fromIndex;
-		var result = -1;
-		for (i in fromIndex...this.length) {
-			if (this.get(i) == searchElement) {
-				result = i;
-				break;
-			}
-		}
-		return result;
-	}
-
-	public inline function subarray(start: Int, ?end: Int) {
-		if (end == null) end = this.length;
-		if (start < 0) start = this.length + start;
-		if (end < 0) end = this.length + end;
-		return new Uint8Array(this.buffer, start * BYTES_PER_ELEMENT, (end - start) * BYTES_PER_ELEMENT);
-	}
-
-	public inline function fill(value: UInt8, ?start: Int = 0, ?end: Int) {
-		if (end == null) end = this.length;
-		if (start < 0) start = this.length + start;
-		if (end < 0) end = this.length + end;
-		for (i in 0...(end - start)) {
-			this.set(i + start, value);
-		}
-	}
-
-	public inline function forEach(callback: UInt8 -> Int -> Uint8Array -> Void): Void {
-		var i = 0;
-		for (v in this) {
-			callback(v, i++, this);
-		}
-	}
-
-	public inline function every(callback: UInt8 -> Int -> Uint8Array -> Bool): Bool {
-		var result: Bool = true;
-		var i = 0;
-		for (v in this) {
-			if (!callback(v, i++, this)) {
-				result = false;
-				break;
-			}
-		}
-		return result;
-	}
-
-	@:arrayAccess
-	@:pure inline function arrayGet(i: Int) {
-		return this.get(i);
-	}
-
-	@:arrayAccess
-	inline function arraySet(i: Int, v: UInt8) {
-		return this.set(i, v);
-	}
-
-	// @:pure static function of( items : haxe.extern.Rest<Array<Dynamic>> ) : Uint8Array {}
-	@:pure static public inline function from(source: ArrayLike<UInt8>, ?mapFn: UInt8 -> Int -> UInt8): Uint8Array {
-		var view = new Uint8Array(source.length);
-		var i = 0;
-		for (v in source) {
-			view[i] = mapFn != null ? mapFn(v, i) : v;
-			i++;
-		}
-		return view;
-	}
-
+	// typed filled by macro
 }
 
 @:noCompletion
 @:nullSafety
-@:allow(typedarray.Uint8Array)
 class Uint8ArrayImpl extends ArrayBufferViewBase {
 
 	static public inline var BYTES_PER_ELEMENT : Int = 1;
@@ -181,62 +56,45 @@ class Uint8ArrayImpl extends ArrayBufferViewBase {
 		);
 	}
 
-	@:pure function get(i: Int) {
+	@:noCompletion
+	@:pure public function _get(i: Int) {
 		#if debug
 		if (i < 0 || i >= this.length) {
 			throw haxe.io.Error.OutsideBounds;
 		}
 		#end
 
-		#if cpp
-		return cpp.NativeArray.unsafeGet(this.nativeBytes, i + this.byteOffset);
-		#else
-		return (this.buffer: haxe.io.Bytes).get(i + this.byteOffset);
-		#end
-	}
-
-	inline function set(i: Int, v: UInt8) {
-		#if debug
-		if (i < 0 || i >= this.length) {
-			throw haxe.io.Error.OutsideBounds;
-		}
-		#end
+		var p = i * BYTES_PER_ELEMENT + this.byteOffset;
 
 		#if cpp
-		return cpp.NativeArray.unsafeSet(this.nativeBytes, i + this.byteOffset, v);
+		return cpp.NativeArray.unsafeGet(this.nativeBytes, p);
 		#else
-		return (this.buffer: haxe.io.Bytes).set(i + this.byteOffset, v);
+		return (this.buffer: haxe.io.Bytes).get(p);
 		#end
-	}
-
-	@:pure public inline function iterator(): Iterator<UInt8> {
-		return new Uint8ArrayIterator(this);
-	}
-
-	function toString() {
-		return [for (i in 0...this.length) get(i)].join(',');
-	}
-
-}
-
-@:access(typedarray.Uint8ArrayImpl)
-private class Uint8ArrayIterator {
-
-	var a: Uint8ArrayImpl;
-	var i: Int = 0;
-
-	public inline function new(array: Uint8ArrayImpl) {
-		this.a = array;
-	}
-
-	public inline function hasNext(): Bool {
-		return i < (this.a.length);
-	}
-
-	public inline function next(): UInt8 {
-		return a.get(i++);
 	}
 	
+	@:noCompletion
+	public inline function _set(i: Int, v: UInt8) {
+		#if debug
+		if (i < 0 || i >= this.length) {
+			throw haxe.io.Error.OutsideBounds;
+		}
+		#end
+
+		var p = i * BYTES_PER_ELEMENT + this.byteOffset;
+
+		#if cpp
+		cpp.NativeArray.unsafeSet(this.nativeBytes, p, v);
+		#else
+		(this.buffer: haxe.io.Bytes).set(p, v);
+		#end
+		return v;
+	}
+
+	@:pure public inline function iterator() {
+		return new ArrayBufferViewImplIterator<UInt8>(this);
+	}
+
 }
 
 #end

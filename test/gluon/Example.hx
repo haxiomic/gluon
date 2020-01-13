6227622270a144cf643e472171517f92abe85419
George Corney <haxiomic@gmail.com>
2020-01-13 18:18:17 +0000

    More interesting example

--- test/gluon/Example.hx
+++ test/gluon/Example.hx
@@ -16,8 +16,13 @@ class Example {
 
     @:keep
     static public function onFrame() {
+        cpp.vm.Gc.run(true);
+
+        var t_s = haxe.Timer.stamp();
+
+        // execute commands on the OpenGL context
+        gl.clearColor(Math.sin(t_s * 0.1), Math.cos(t_s * 0.5), Math.sin(t_s * 0.3), 1);
         gl.clear(COLOR_BUFFER_BIT);
-        gl.clearColor(0, 1, 0, 1);
     }
 
 }

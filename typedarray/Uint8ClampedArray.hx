288eaa158fa4e6889cb0634cd08c8ba174367f95
nanjizal <nanjizal@users.noreply.github.com>
2021-02-12 16:12:32 +0000

    Update Uint8ClampedArray.hx

--- typedarray/Uint8ClampedArray.hx
+++ typedarray/Uint8ClampedArray.hx
@@ -16,7 +16,7 @@ private typedef UInt8 = cpp.UInt8;
 private typedef UInt8 = Int;
 #end
 
-
+@:transitive
 @:nullSafety
 @:forward
 #if !macro
@@ -30,6 +30,7 @@ abstract Uint8ClampedArray(Uint8ClampedArrayImpl)
 	// typed filled by macro
 }
 
+@:transitive
 @:noCompletion
 @:nullSafety
 class Uint8ClampedArrayImpl extends ArrayBufferViewBase {
